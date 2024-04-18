41fc674011c1864253b22dc211f628c8120c78d4
DianaGMD <a01368899@tec.mx>
2024-04-18 13:29:10 -0600

    Precio del producto limpio

--- Clientes_RFC.py
+++ Clientes_RFC.py
@@ -8,7 +8,7 @@ print(clientes_df.head())
 print("\nSuma de valores nulos:")
 print(clientes_df.isnull().sum())
 
-def valores_faltantes(df):
+def rellenar_valores_faltantes(df):
     # RFC Generico
     df['RFC'].fillna('XAXX010101000', inplace=True)
     
@@ -18,7 +18,7 @@ def valores_faltantes(df):
     return df
 
 if __name__ == "__main__":
-    clientes_df = valores_faltantes(clientes_df)
+    clientes_df = rellenar_valores_faltantes(clientes_df)
     
 
     clientes_df.to_csv('clientes_RFCNull.csv', index=False)
---
+++ Precios_Productos.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_excel('precios_productos.xlsx')
+print(df.head())
+
+def rellenar_valores_faltantes(df):
+    df['NOMBRE_VENDEDOR'].fillna("Sin Asignar", inplace=True)
+    return df
+
+df = rellenar_valores_faltantes(df)
+df.to_csv('precio_productos_tratado(limpio).csv', index=False)
+print("\nLimpieza de valores nulos::")
+print(df.info())
+print(df.head())
